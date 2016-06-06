026708d0913f1e5d31dfa9dc1555498e8344baa6
ibogdanov <ibogdanov@salesforce.com>
2016-06-05 18:24:23 -0700

    cleanup

--- src/refs.js
+++ src/refs.js
@@ -23,7 +23,7 @@
  * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
  * POSSIBILITY OF SUCH DAMAGE.
  */
- 
+
 import union from 'lodash.union';
 import remove from 'lodash.remove';
 import trim from 'lodash.trim';
@@ -54,7 +54,3 @@ const notify = (ids,sobjs,compactLayout,defaultLayout,doRefs) => {
 };
 
 query.addListener(notify);
-
-module.exports = {
-
-};
